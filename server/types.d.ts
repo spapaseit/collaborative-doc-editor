81480cb77a1fd23d7823edf1b314196aafb746a3
Sergi Papaseit Valls <sergi@codenamed.dev>
2024-07-03 12:47:47 +0200

    Initial server setup

---
+++ server/types.d.ts
@@ -0,0 +1,6 @@
+// declare module "y-websocket/bin/utils" {
+//     import { WebSocket } from "ws";
+//     import * as Y from "yjs";
+
+//     export function setupWSConnection(conn: WebSocket, doc: Y.Doc, options?: { [key: string]: any }): void;
+// }
